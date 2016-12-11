1c571d78ff7760e8355b0bb76ad574482532fc0d
Phillips <calum.phillips@CHAMPLAIN.EDU>
2016-12-10 20:28:45 -0500

    Change is Great!
    Did work on making the house interior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,34 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private HouseManager manager;
+	private Rigidbody2D rigid;
 
-	public int maxSpeed = 3;
+	public float speed = 3;
 
 	void Start () {
 		manager = GameObject.FindGameObjectWithTag ("GameController").GetComponent<HouseManager> ();
-
+		rigid = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
 		//Movement
 		if (!manager.isControlingHouse ()) {
+			//Testing collison
+			//NOT FINAL MOVEMENT
+			float x = 0, y = 0;
+			if (Input.GetKey (KeyCode.W))
+				y = speed;
+			else if (Input.GetKey (KeyCode.S))
+				y = -speed;
 
+			if (Input.GetKey (KeyCode.D))
+				x = speed;
+			else if (Input.GetKey (KeyCode.A))
+				x = -speed;
+
+			rigid.velocity = new Vector2 (x, y);
+
+			//MAKE PLAYER LOOK TORWARDS MOUSE
 		}
 
 		//Taking Damage and such, collision boxes.
